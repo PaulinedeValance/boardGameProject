b3ba2ac7263b1ff9240e7211cecefbedddebfa41
PaulinedeValance <pauline.de.valance@gmail.com>
2023-02-07 17:39:49 +0100

    clean up the files

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<template>
+    <nav>
+        <ul class="navbar-display">
+            <li class="navbar-link"><router-link to="/">Home</router-link></li>
+            <li class="navbar-link"><router-link to="/favoris">Favoris</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+.navbar-display {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.navbar-link {
+    list-style: none;
+    padding: 20px;
+
+}
+
+a {
+    text-decoration: none;
+}
+</style>
---
+++ src/views/Favoris.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>Page de favoris</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
