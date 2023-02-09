1d3245b9d6ee8ffbf220c79649d89d56adf7830d
PaulinedeValance <pauline.de.valance@gmail.com>
2023-02-09 16:55:35 +0100

    api call with name and image

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,25 @@
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import Input from "../components/Input.vue"
 import Button from "../components/Button.vue"
+import { useAxios } from '@vueuse/integrations/useAxios'
+
+interface Game {
+  id: string;
+  handle: string
+  name: string;
+  image_url: string
+  image: string
+}
+
+const gamesList = ref<Game[]>([])
+const getData = async () => {
+  const { data } = await useAxios('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id=JLBr5npPhV')
+  console.log(data.value.games)
+  const games = data.value.games.map((game: Game) => ({ id: game.id, name: game.handle, image: game.image_url }))
+  gamesList.value = games
+}
+getData()
 
 const newGame = ref<string>('')
 const eraseText = ref<string>('')
@@ -29,6 +46,10 @@ const deleteSpaceInput = () => {
 
 <template>
   <h1>Rechercher un jeu</h1>
+  <!-- <div v-for="game in games"> name : {{ game.name.toUpperCase() }} date de sortie : {{ game.releaseDate }}</div> -->
+  <div v-for="game in gamesList" :key="game.handle">{{ game.name }} - <img class="img-api" :src="game.image" />
+  </div>
+
   <div>
     <Input v-model="newGame" :placeholder="'Recherchez un jeu'" />
     <Button @clicked="logger()" :name="'Valider'" />
@@ -51,6 +72,10 @@ const deleteSpaceInput = () => {
 </template>
 
 <style scoped>
+.img-api {
+  width: 20%;
+}
+
 h1 {
   font-size: 22px;
   padding-left: 20px;
