233a1099f494b8cfd13633b04d3e310093d0c05f
PaulinedeValance <pauline.de.valance@gmail.com>
2023-02-20 13:56:55 +0100

    error message emptu input

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,18 +13,26 @@ export interface Game {
   image: string
 }
 
+// store the value of the search input
 const searchedGame = ref<string>('')
+// store the list of games
 const gamesList = ref<Game[]>([])
+const errorMessage = ref('')
 
+// fetch the api with an async function
 const getData = async () => {
   if (searchedGame.value === "") {
-    alert("Please, enter a game")
+    errorMessage.value = "Please enter a game"
     return
   }
   const { data } = await useAxios(`https://api.boardgameatlas.com/api/search?name=${searchedGame.value}&fuzzy_match=true&client_id=JLBr5npPhV`)
 
+  // map over the datas and keep the games name and images url
   const games = data.value.games.map((game: Game) => ({ name: game.handle, image: game.image_url }))
-  gamesList.value = games
+  // update the gamesList with the list of games 
+  gamesList.value = games;
+  // clear the input value after the button is clicked
+  searchedGame.value = ''
 }
 
 </script>
@@ -35,6 +43,7 @@ const getData = async () => {
   <div>
     <Input v-model="searchedGame" :placeholder="'Recherchez un jeu'" />
     <Button @clicked="getData()" :name="'Valider'" />
+    <p class="error-message" v-if="errorMessage">{{ errorMessage }}</p>
 
     <div class="container-flex">
       <div class="container-game" v-for="game in gamesList" :key="game.handle">
@@ -69,5 +78,15 @@ h1 {
   padding-left: 20px;
   padding-top: 20px;
   color: #1e4975;
+}
+
+.error-message {
+  font-size: 18px;
+  font-family: 'Didact Gothic', sans-serif;
+  color: crimson;
+  padding-left: 32px;
+  padding-top: 4px;
+  margin: 0;
+
 }
 </style>
